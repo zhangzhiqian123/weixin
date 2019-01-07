56de96b819a8459d3cf9634793239eb6cf1c00d1
zhiqian <821011270@qq.com>
2019-01-07 23:05:12 +0800

    first commit weixin

---
+++ src/pages/proList.js
@@ -0,0 +1,65 @@
+export const proList = [
+  {
+    "id": 14,
+    "opt_id": 14,
+    "opt_name": "服饰",
+    "opt_desc": "一级",
+    "image_url": "http://omsproductionimg.yangkeduo.com/goods/5635bbe762/730/100/6bce5c6db8e6bf1a41b2544d453a255c.jpg",
+    "selected_url": "http://t05img.yangkeduo.com/images/2018-03-09/d444a41c7dd439681be3310f8fc3dce8.png",
+    "unselected_url": "http://omsproductionimg.yangkeduo.com/images/2017-06-04/da66477f08f39bbb5ae6d245176e3799.png",
+    "priority": 1,
+    "is_highlight": 0,
+    "opt_id_1": 14,
+    "opt_id_2": 0,
+    "opt_id_3": 0,
+    "man_priority": 4,
+    "children": [
+      {
+        "id": 45,
+        "opt_id": 45,
+        "opt_name": "女装",
+        "opt_desc": "服饰二级",
+        "image_url": "http://t07img.yangkeduo.com/images/2018-04-25/97cedd15e01364cd56bfb7d55e9c8e92.png",
+        "priority": 1,
+        "is_highlight": 1,
+        "opt_id_1": 14,
+        "opt_id_2": 45,
+        "opt_id_3": 0,
+        "man_priority": 0
+      },
+
+    ]
+  },
+  {
+    "id": 1281,
+    "opt_id": 1281,
+    "opt_name": "鞋包",
+    "opt_desc": "一级",
+    "image_url": "http://omsproductionimg.yangkeduo.com/images/2017-05-16/bd76b6c9874a3f5dafc5f41607bbc4a1.jpeg",
+    "selected_url": "http://t09img.yangkeduo.com/images/2018-03-09/4748728fc8057b5a6aba87dba581ee94.png",
+    "unselected_url": "http://omsproductionimg.yangkeduo.com/images/2017-06-04/8d43d4891e4526d1a99983f7c0d698c0.png",
+    "priority": 2,
+    "is_highlight": 0,
+    "opt_id_1": 1281,
+    "opt_id_2": 0,
+    "opt_id_3": 0,
+    "man_priority": 2,
+    "children": [
+      {
+        "id": 1285,
+        "opt_id": 1285,
+        "opt_name": "凉鞋",
+        "opt_desc": "鞋包二级",
+        "image_url": "http://t00img.yangkeduo.com/t01img/images/2018-06-08/02eb7c92b024d133b0e43fbbddc51777.jpeg",
+        "priority": 1,
+        "is_highlight": 0,
+        "opt_id_1": 1281,
+        "opt_id_2": 1285,
+        "opt_id_3": 0,
+        "man_priority": 0
+      },
+    ]
+  },
+
+
+]
